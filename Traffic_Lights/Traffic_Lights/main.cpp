6d6990c33e2aad8b53db08617dfd3095060f3ee4
Anas Saeed <anassaeed72@gmail.com>
2015-12-14 19:45:38 +0500

    Traffic_Lights

---
+++ Traffic_Lights/Traffic_Lights/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Traffic_Lights
+//
+//  Created by Anas Saeed on 05/06/2014.
+//  Copyright (c) 2014 Anas Saeed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
